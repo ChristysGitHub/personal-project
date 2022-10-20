6b4a86566fcc02b9c86fab11185a5afc15b00ae0
ChristysGitHub <chrndaye@gmail.com>
2022-10-19 23:06:05 -0700

    connecting project folder to github repo

---
+++ backend/src/main/java/org/tbfac/securitymanagement/SecuritymanagementApplication.java
@@ -0,0 +1,13 @@
+package org.tbfac.securitymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritymanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuritymanagementApplication.class, args);
+	}
+
+}
